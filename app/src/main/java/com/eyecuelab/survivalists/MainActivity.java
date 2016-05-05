fd31b070ec1083347ce5642980e3d69588b70c06
NathanRomike <nathanromike@gmail.com>
2016-05-04 18:29:33 -0700

    Working on google play login, not working yet.

--- app/src/main/java/com/eyecuelab/survivalists/MainActivity.java
+++ app/src/main/java/com/eyecuelab/survivalists/MainActivity.java
@@ -1,26 +1,43 @@
 package com.eyecuelab.survivalists;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.example.games.basegameutils.BaseGameUtils;
+
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     @Bind(R.id.stepTextView) TextView counter;
 
     private SensorManager sensorManager;
     boolean activityRunning;
+
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +53,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         ButterKnife.bind(this);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
+
+        mGoogleApiClient.connect();
+
+
     }
 
     @Override
@@ -46,6 +72,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (countSensor != null) {
             sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
         } else {
+            Log.d("Bug", "Hardware pedometer didn't work");
             Toast.makeText(this, "Hardware counter isn't available, utilizing custom!", Toast.LENGTH_LONG).show();
         }
     }
@@ -66,4 +93,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (connectionHint != null) {
+            Toast.makeText(this, "Connected to google api", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "Connection suspended, reconnecting", Toast.LENGTH_LONG).show();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(this, "Connection failed", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        BaseGameUtils.showActivityResultError(this, requestCode,resultCode, R.string.sign_in_failed);
+    }
 }
