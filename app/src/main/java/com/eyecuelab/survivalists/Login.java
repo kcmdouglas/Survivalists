5e224ebe5e1834556c689a7eacac934a491b2676
Kassidy Douglas <kcmdouglas@gmail.com>
2016-05-10 13:29:00 -0700

    Add User object and Login activity.

--- app/src/main/java/com/eyecuelab/survivalists/Login.java
+++ app/src/main/java/com/eyecuelab/survivalists/Login.java
@@ -3,11 +3,11 @@ package com.eyecuelab.survivalists;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FirebaseLogin extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_firebase_login);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/eyecuelab/survivalists/User.java
@@ -0,0 +1,19 @@
+package com.eyecuelab.survivalists;
+
+/**
+ * Created by eyecuelab on 5/10/16.
+ */
+public class User {
+    public String email;
+
+    public User() {
+    }
+
+    public User (String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
