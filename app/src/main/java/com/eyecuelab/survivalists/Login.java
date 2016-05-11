82078c4e02e43570d67ced15c7dacb394c0278e0
Kassidy Douglas <kcmdouglas@gmail.com>
2016-05-10 17:13:47 -0700

    Update User class

--- app/src/main/java/com/eyecuelab/survivalists/Login.java
+++ app/src/main/java/com/eyecuelab/survivalists/Login.java
@@ -1,13 +1,32 @@
 package com.eyecuelab.survivalists;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class Login extends AppCompatActivity {
+
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.loginButton) Button mLoginButton;
+    @Bind(R.id.registerButton) Button mRegisterButton;
+
+    private Firebase mFirebaseRef;
+    private ProgressDialog mAuthProgressDialog;
+    private Firebase.AuthResultHandler mAuthResultHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
     }
 }
