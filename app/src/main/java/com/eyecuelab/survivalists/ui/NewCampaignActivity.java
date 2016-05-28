2408f72f01491c627ba76ddc76025598ad64b2ca
NathanRomike <nathanromike@gmail.com>
2016-05-27 17:59:57 -0700

    Tweaks to the send invitation layout.

--- app/src/main/java/com/eyecuelab/survivalists/ui/NewCampaignActivity.java
+++ app/src/main/java/com/eyecuelab/survivalists/ui/NewCampaignActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.eyecuelab.survivalists.R;
 
@@ -22,15 +23,18 @@ import butterknife.ButterKnife;
 public class NewCampaignActivity extends AppCompatActivity implements View.OnClickListener {
     private int mDifficultyLevel;
     private int mCampaignLength;
+    private int mPartySize;
     private boolean mConfirmingSettings = true;
     private ArrayList<String> descriptions = new ArrayList<>();
     private ArrayList<String> lengths = new ArrayList<>();
+    private ArrayList<String> invitedPlayers = new ArrayList<>();
 
     private ListView mInvitePlayersListView;
     private Context mContext;
 
     @Bind(R.id.difficultySeekBar) SeekBar difficultySeekBar;
     @Bind(R.id.campaignLengthSeekBar) SeekBar lengthSeekBar;
+    @Bind(R.id.partySizeSeekBar) SeekBar partySeekBar;
     @Bind(R.id.difficultyDescription) TextView difficultyTextView;
     @Bind(R.id.lengthText) TextView lengthTextView;
     @Bind(R.id.invitePlayersListView) ListView invitePlayerListView;
@@ -42,6 +46,7 @@ public class NewCampaignActivity extends AppCompatActivity implements View.OnCli
     @Bind(R.id.teamBuildingSection) PercentRelativeLayout playerInvitationLayout;
     @Bind(R.id.difficultyConfirmedText) TextView difficultyConfirmedTextView;
     @Bind(R.id.lengthConfirmedText) TextView lengthConfirmedTextView;
+    @Bind(R.id.partySizeText) TextView partyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,9 +85,12 @@ public class NewCampaignActivity extends AppCompatActivity implements View.OnCli
             case R.id.confirmationButton:
                 if (mConfirmingSettings == true) {
                     loadAvailablePlayers();
+                } else if (mPartySize == invitedPlayers.size()){
+                    sendInvitations();
+                    Toast.makeText(NewCampaignActivity.this, "Invitations sent", Toast.LENGTH_LONG).show();
                 } else {
-                    //Send invitations
-
+                    int remainingInvites = mPartySize - invitedPlayers.size();
+                    Toast.makeText(NewCampaignActivity.this, "You need to select " + remainingInvites + " more players.", Toast.LENGTH_LONG).show();
                 }
                 break;
         }
@@ -94,18 +102,28 @@ public class NewCampaignActivity extends AppCompatActivity implements View.OnCli
         settingConfirmationLayout.setVisibility(View.VISIBLE);
         generalInfoLayout.setVisibility(View.GONE);
         playerInvitationLayout.setVisibility(View.VISIBLE);
-        confirmationButton.setText("Send Invitations");
+
+        int remainingInvites = mPartySize - invitedPlayers.size();
+        confirmationButton.setText(remainingInvites + " invitations remaining...");
 
         difficultyConfirmedTextView.setText("Difficulty: " + descriptions.get(mDifficultyLevel));
         lengthConfirmedTextView.setText("Length: " + lengths.get(mCampaignLength) + " Days");
 
         String[] players = new String[] {"This Nose Knows", "Hello", "Testing", "This Nose Knows", "Hello", "Testing", "This Nose Knows", "Hello", "Testing",};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.player_list_item, R.id.playerNameTextView, players);
+
         invitePlayerListView.setAdapter(adapter);
         invitePlayerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {}
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                invitedPlayers.add(String.valueOf(position));
+            }
         });
+    }
+
+    public void sendInvitations() {
+        confirmationButton.setText("Awaiting Responses...");
+        confirmationButton.setTextColor(getColor(android.R.color.darker_gray));
     }
 
     public void initiateSeekBars() {
@@ -117,12 +135,12 @@ public class NewCampaignActivity extends AppCompatActivity implements View.OnCli
                 progressTotal = progress;
                 difficultyTextView.setText(descriptions.get(progress));
             }
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                difficultyTextView.setText(descriptions.get(progressTotal));
                 mDifficultyLevel = progressTotal;
             }
         });
@@ -132,7 +150,6 @@ public class NewCampaignActivity extends AppCompatActivity implements View.OnCli
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
                 lengthTextView.setText(lengths.get(progress) + " Days");
                 progressTotal = progress;
             }
@@ -142,8 +159,27 @@ public class NewCampaignActivity extends AppCompatActivity implements View.OnCli
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                lengthTextView.setText(lengths.get(progressTotal) + " Days");
                 mCampaignLength = progressTotal;
+            }
+        });
+
+        partySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressTotal = 2;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int currentCount = progress + 2;
+                partyTextView.setText(currentCount + " Players");
+                progressTotal = progress + 2;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mPartySize = progressTotal;
+                Toast.makeText(NewCampaignActivity.this, "" + mPartySize, Toast.LENGTH_LONG).show();
             }
         });
     }
